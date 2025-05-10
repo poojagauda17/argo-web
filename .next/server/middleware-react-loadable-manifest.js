d4ad4cabd854d3c120f53e6787128328bfba44eb
poojagauda17 <poojagauda17@gmail.com>
2025-05-11 01:52:04 +0530

    Updated latest build

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST='{"..\\\\node_modules\\\\next\\\\dist\\\\client\\\\index.js -> ../pages/_app":{"id":472,"files":["static/chunks/472.ab218e9bb4daed81.js"]},"..\\\\node_modules\\\\next\\\\dist\\\\client\\\\index.js -> ../pages/_error":{"id":9341,"files":["static/chunks/341.a4db87b0221bc7d3.js"]}}';
